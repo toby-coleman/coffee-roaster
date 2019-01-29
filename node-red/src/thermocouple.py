5754e677019edb1c1ff03f9d0c851c19e332d7b7
Toby Coleman <toby@tobycoleman.com>
2019-01-29 21:51:16 +0000

    Improved thermocouple readings

--- node-red/src/thermocouple.py
+++ node-red/src/thermocouple.py
@@ -1,11 +1,23 @@
-import Adafruit_GPIO.SPI as SPI
+import argparse
 import Adafruit_MAX31855.MAX31855 as MAX31855
 
 
-# Raspberry Pi software SPI configuration.
-CLK = 25
-CS  = 24
-DO  = 18
-sensor = MAX31855.MAX31855(CLK, CS, DO)
-# Outpu CSV format
-print('{0},{1}'.format(sensor.readTempC(), sensor.readInternalC()))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Read thermocouple')
+    parser.add_argument('--clk', default=25, type=int, help='Clock')
+    parser.add_argument('--cs', default=24, type=int, help='Chip select')
+    parser.add_argument('--do', default=18, type=int, help='Data out')
+    parser.add_argument('--samples', default=1, type=int, help='Number of samples')
+    args = parser.parse_args()
+
+    sensor = MAX31855.MAX31855(args.clk, args.cs, args.do)
+
+    internal_temp = sensor.readInternalC()
+    samples = [sensor.readTempC() for x in range(args.samples)]
+    if len(samples) > 3:
+        # Take trimmed mean
+        samples = sorted(samples)[1:-1]
+    thermocouple_temp = sum(samples) / len(samples)
+    
+    # Print output in CSV format
+    print('{0},{1}'.format(thermocouple_temp, internal_temp))
